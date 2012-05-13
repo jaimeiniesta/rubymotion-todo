445470aed16f55043fead4810bd723fc69ed0e94
Jaime Iniesta <jaimeiniesta@gmail.com>
2012-05-13 18:47:29 +0200

    Use a UISearchBar to enter new items

--- app/items_controller.rb
+++ app/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < UITableViewController
   def viewDidLoad
     @items = ['work', 'clean house', 'go to the park', 'have dinner', 'sleep']
+
+    searchBar = UISearchBar.alloc.initWithFrame(CGRectMake(0, 0, self.tableView.frame.size.width, 0))
+    searchBar.delegate = self;
+    searchBar.showsCancelButton = true;
+    searchBar.sizeToFit
+    view.tableHeaderView = searchBar
   end
 
   def tableView(tableView, numberOfRowsInSection:section)
@@ -9,5 +15,16 @@ class ItemsController < UITableViewController
 
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
     ItemCell.cellForItem(@items[indexPath.row], inTableView:tableView)
+  end
+
+  def searchBarSearchButtonClicked(searchBar)
+    @items << searchBar.text
+    view.reloadData
+    searchBar.text = nil
+    searchBar.resignFirstResponder
+  end
+
+  def searchBarCancelButtonClicked(searchBar)
+    searchBar.resignFirstResponder
   end
 end
