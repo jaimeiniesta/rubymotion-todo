64e993df1d7fb4b09df21667d9e8d19b0582ec87
Francis Chong <francis@ignition.hk>
2012-05-15 08:58:03 +0800

    store all items in memory rather than query them every time

--- app/items_controller.rb
+++ app/items_controller.rb
@@ -1,25 +1,33 @@
 class ItemsController < UITableViewController
+  def load
+    @item = Item.all.sort { |a,b| a.row.to_i <=> b.row.to_i }
+    self.view.reloadData
+  end
+  
   def viewDidLoad
     searchBar = UISearchBar.alloc.initWithFrame(CGRectMake(0, 0, self.tableView.frame.size.width, 0))
     searchBar.delegate = self;
     searchBar.showsCancelButton = true;
     searchBar.sizeToFit
     view.tableHeaderView = searchBar
+    load
+  end
+  
+  def viewDidUnload
+    @item = nil
   end
 
   def tableView(tableView, numberOfRowsInSection:section)
-    Item.all.size
+    @item.size
   end
 
-  # TODO: find the item using Item.find(:row)
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    item = Item.all.sort { |a,b| a.row.to_i <=> b.row.to_i }[indexPath.row]
-    ItemCell.cellForItem(item, inTableView:tableView)
+    ItemCell.cellForItem(@item[indexPath[1]], inTableView:tableView)
   end
 
   def searchBarSearchButtonClicked(searchBar)
     Item.create(:title => searchBar.text, :row => Item.all.size)
-    view.reloadData
+    load
     searchBar.text = nil
     searchBar.resignFirstResponder
   end
